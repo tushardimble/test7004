1d6bf2307fb47ad2fda3b4177376b1848617865d
tushardimble <dimbletushar7004@gmail.com>
2020-01-30 13:50:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,28 @@
 
    $input = json_decode(file_get_contents('php://input'), true);
    $param = $input["result"]["parameters"]["text"];
-   echo $param;exit;
+   switch ($param) {
+      
+       case 'hi':
+           $speech = "Hi, nice to meet you";
+       break;
+
+       case 'bye':
+           $speech = "Good night";
+       break;
+        
+        case 'anything':
+           $speech = "Yes type anything here";
+       break;
+
+       default:
+           $speech = "sorry didn't get.";
+           break;
+   }
+   $response = new \stdClass();
+   $response->speech=$speech;
+   $response->displayText=$speech;
+   $response->source="webhook";
+   echo json_encode($response);
    
 ?>
