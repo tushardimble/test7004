6bc5b74f374c38328d63e67fe6f83bff8938623b
tushardimble <dimbletushar7004@gmail.com>
2020-01-30 18:16:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,66 +24,14 @@ if($method == 'POST'){
            $speech = "sorry didn't get.";
            break;
    }
-   //$response = new \stdClass();
-   //$response->speech= $speech;
-   //$response->displayText= $speech;
-   //$response->source= "webhook";
-   $response = {
-  "responseId": "cb6f5ec2-c26e-4349-b561-a9ddd6a0e495",
-  "queryResult": {
-    "queryText": "actions_intent_CONFIRMATION",
-    "action": "Dialogflow action name of matched intent",
-    "parameters": {},
-    "allRequiredParamsPresent": true,
-    "outputContexts": [
-      {
-        "name": "projects/${PROJECTID}/agent/sessions/${SESSIONID}/contexts/actions_intent_confirmation",
-        "parameters": {
-          "CONFIRMATION": true
-        }
-      }
-    ],
-    "intent": {
-      "name": "projects/${PROJECTID}/agent/intents/1777d616-a5f7-4838-a9a9-870f2956bd14",
-      "displayName": "Dialogflow action name of matched intent"
-    },
-    "intentDetectionConfidence": 1,
-    "diagnosticInfo": {},
-    "languageCode": "en-us"
-  },
-  "originalDetectIntentRequest": {
-    "source": "google",
-    "version": "2",
-    "payload": {
-      "isInSandbox": true,
-      "surface": {},
-      "inputs": [
-        {
-          "rawInputs": [
-            {
-              "query": "yes",
-              "inputType": "VOICE"
-            }
-          ],
-          "arguments": [
-            {
-              "name": "CONFIRMATION",
-              "boolValue": true
-            }
-          ],
-          "intent": "actions.intent.CONFIRMATION"
-        }
-      ],
-      "user": {},
-      "conversation": {},
-      "availableSurfaces": []
-    }
-  },
-  "session": "projects/${PROJECTID}/agent/sessions/${SESSIONID}"
-}
+   $response = new \stdClass();
+   $response->speech= $speech;
+   $response->displayText= $speech;
+   $response->source= "webhook";
+   
    ob_end_clean();
-   echo $response;
-//    echo json_encode($response);
+   
+    echo json_encode($response);
   }
 else
 {
