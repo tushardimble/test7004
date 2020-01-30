4a980911f10719f12a79d9f58ec98b31f186f46e
tushardimble <dimbletushar7004@gmail.com>
2020-01-30 15:08:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
    $input = json_decode(file_get_contents('php://input'), true);
-   $param = $input["result"]["parameters"]["text"];
+   $param = $input["queryResult"]["queryText"];
    switch ($param) {
       
        case 'hi':
