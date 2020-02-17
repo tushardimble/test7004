5f4a5bcbfece93f1ad4c099ab9fae8f022aeb66f
tushardimble <dimbletushar7004@gmail.com>
2020-02-17 18:36:32 +0530

    Update common.php

--- ajax/common.php
+++ ajax/common.php
@@ -1,4 +1,5 @@
 <?php
 
-	$basepath = $_SERVER['HTTP_HOST']."/axisbankgooglebasedbot/";
+	//$basepath = $_SERVER['HTTP_HOST']."/axisbankgooglebasedbot/";
+	$basepath = "https://dialogflow7004.herokuapp.com/";
 ?>
