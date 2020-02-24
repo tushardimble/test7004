384247840da24996514030a6a502dc4fbfd60d34
tushardimble <dimbletushar7004@gmail.com>
2020-02-24 18:00:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
-	$jsoninput  = file_get_contents('php://input');
+	//$jsoninput  = file_get_contents('php://input');
 	// Converts it into a PHP object
-	$data = json_decode($json);
+	//$data = json_decode($json);
 	//echo $data['queryResult']['parameters']['Account_Number'];
-	$data1['output'] = "Please Enter valid Mobile Number";
+	//$data1['output'] = "Please Enter valid Mobile Number";
+	$json = {
+	  "fulfillmentText": "Text response",
+	  "fulfillmentMessages": [
+	    {
+	      "text": {
+		"text": ["Text response"]
+	      }
+	    }
+	  ],
+	  "source": "<Text response>"
+	}
 	
 ?>
