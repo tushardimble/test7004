78e8cfe02258aab00dab51de388a21795ba86654
tushardimble <dimbletushar7004@gmail.com>
2020-02-24 18:07:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,70 @@
 <?php
-	//$jsoninput  = file_get_contents('php://input');
-	// Converts it into a PHP object
-	//$data = json_decode($json);
-	//echo $data['queryResult']['parameters']['Account_Number'];
-	//$data1['output'] = "Please Enter valid Mobile Number";
-	$json = {
-	  "fulfillmentText": "Text response",
-	  "fulfillmentMessages": [
-	    {
-	      "text": {
-		"text": ["Text response"]
-	      }
-	    }
-	  ],
-	  "source": "<Text response>"
-	}
+	$data = array (
+  'responseId' => '5b9aba54-91ba-4af6-9d21-7f775e8848a0-7f245a81',
+  'queryResult' => 
+  array (
+    'queryText' => '456 123',
+    'action' => 'BalanceRequest.BalanceRequest-yes.BalanceRequest-yes-custom',
+    'parameters' => 
+    array (
+      'Account_Number' => 456123,
+    ),
+    'allRequiredParamsPresent' => true,
+    'fulfillmentText' => 'Please enter the Valid Mobile Number linked to Account?',
+    'fulfillmentMessages' => 
+    array (
+      0 => 
+      array (
+        'text' => 
+        array (
+          'text' => 
+          array (
+            0 => 'Please enter the Valid Mobile Number linked to Account?',
+          ),
+        ),
+      ),
+    ),
+    'outputContexts' => 
+    array (
+      0 => 
+      array (
+        'name' => 'projects/bankscustomersupport-mnlxxa/agent/sessions/527c9946-cd74-8e76-f1f8-766df5915f91/contexts/balancerequest-yes-custom-followup',
+        'lifespanCount' => 2,
+        'parameters' => 
+        array (
+          'Account_Number.original' => '456 123',
+          'Account_Number' => 456123,
+        ),
+      ),
+      1 => 
+      array (
+        'name' => 'projects/bankscustomersupport-mnlxxa/agent/sessions/527c9946-cd74-8e76-f1f8-766df5915f91/contexts/balancerequest-yes-followup',
+        'lifespanCount' => 1,
+        'parameters' => 
+        array (
+          'Account_Number.original' => '456 123',
+          'Account_Number' => 456123,
+        ),
+      ),
+    ),
+    'intent' => 
+    array (
+      'name' => 'projects/bankscustomersupport-mnlxxa/agent/intents/a4c4ef15-5fbb-4ab2-91d6-ceae1af4a8a3',
+      'displayName' => 'BalanceRequest - yes - AccountNumber',
+    ),
+    'intentDetectionConfidence' => 0.99899966,
+    'diagnosticInfo' => 
+    array (
+      'webhook_latency_ms' => 72,
+    ),
+    'languageCode' => 'en',
+  ),
+  'webhookStatus' => 
+  array (
+    'code' => 14,
+    'message' => 'Webhook call failed. Error: UNAVAILABLE.',
+  ),
+);
+
 	
 ?>
