495b9ee5eb3570bff3f5c53669882bf35b2295ba
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 17:03:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-error_reporting(E_ALL);
+error_reporting(0);
+	define('DB_SERVER','66.45.232.178');
+	define('DB_USER','axisbankcrm');
+	define('DB_PASS' ,'axisbankcrm');
+	define('DB_NAME', 'axisbankcrm');
+
  $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 			// Check connection
 			if (mysqli_connect_errno()){
