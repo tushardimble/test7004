c110726e042f2063c1753a3329625a6cd5d4d837
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 14:18:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php
- $json = file_get_contents('php://input');
-  $b = json_decode($json);
-  $account_number = $b->queryResult->parameters->Account_Number;
- if($account_number == "456123"){
-   $message = "Please Enter mobile number associated with account";
- }else{
-  $message = "Sorry we could not found any details against this account number";
+define('DB_SERVER','66.45.232.178');
+	define('DB_USER','assistant');
+	define('DB_PASS' ,'assistant');
+	define('DB_NAME','assistant');
+ $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+ if (mysqli_connect_errno()){
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}else{
+  echo"connected";
  }
-  $data = array (
-   'fulfillmentText' => $message
-  );
-  $a = json_encode($data);
+ //$json = file_get_contents('php://input');
+  //$b = json_decode($json);
+  //$account_number = $b->queryResult->parameters->Account_Number;
+// if($account_number == "456123"){
+ //  $message = "Please Enter mobile number associated with account";
+ //}else{
+ // $message = "Sorry we could not found any details against this account number";
+ //}
+ // $data = array (
+ //  'fulfillmentText' => $message
+  //);
+ // $a = json_encode($data);
   
-  print_r($a);
+ // print_r($a);
 ?>
