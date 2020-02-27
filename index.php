cbd799121aca776aa87f5c5a0b19566485c3be4f
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 17:02:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,12 @@
 <?php
 error_reporting(E_ALL);
-  include("DBmanager.php");
-    $db     =   new DbManager();
-	$accountNumber = "456123"
-
-	$columnName = "CONCAT(vcd.firstname,' ',vcd.lastname) AS name , vcscf.cf_864 as account_balance";
-	$tableName = "vtiger_contactdetails vcd";
-	$condition = "vce.deleted=0 AND vcscf.cf_856=$accountNumber";
-	$join = "JOIN vtiger_crmentity vce ON vcd.contactid=vce.crmid JOIN vtiger_contactscf vcscf ON vcd.contactid=vcscf.contactid";
-	$sOrderBy = "ORDER BY vcd.contactid DESC";
-	$data = $db -> getDataByJoin($columnName,$tableName,$condition,$join,$sOrderBy);
-
-	if(count($data) > 0){
-		$response['status'] = 200;
-		$response['message'] = "Account Balance";
-		$response['data'] = "available";
-		echo json_encode($response);
-	}else{
-		$response['status'] = 400;
-		$response['message'] = "Not Found";
-		echo json_encode($response);
-	}
+ $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+			// Check connection
+			if (mysqli_connect_errno()){
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		 	}else{
+				echo"connected";
+			}
 
 
   
