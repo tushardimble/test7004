20422d2deb4c8b595f3a2bda45d6776af14a7706
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 13:20:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+  $connection = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
   $data = array();
   $data = array (
-    'fulfillmentText' => 'Please enter the Valid Mobile Number ?'
+    'fulfillmentText' => 'hI THIS IS TEST'
   );
   $a = json_encode($data);
   echo $a; 
