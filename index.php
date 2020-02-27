760bf694ce82ca1c591c2d59f6a5f81ba4eee594
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 17:06:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
-error_reporting(0);
-	define('DB_SERVER','66.45.232.178:8880');
-	define('DB_USER','axisbankcrm');
-	define('DB_PASS' ,'axisbankcrm');
-	define('DB_NAME', 'axisbankcrm');
 
- $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
-			// Check connection
-			if (mysqli_connect_errno()){
-				echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		 	}else{
-				echo"connected";
-			}
+  $request = file_get_contents('php://input');
+  $requestDecode = json_decode($request);
 
+  $intent = $requestDecode->queryResult->intent->displayName;
+  $account_number = $requestDecode->queryResult->parameters->Account_Number;
+
+  if($intent == "BalanceRequest - yes - AccountNumber"){
+    if($account_number == "456123"){
+      $message = "Please Enter mobile number associated with account";
+    }else{
+    $message = "Sorry we could not found any details against this account number";
+    }
+    $data = array (
+      'fulfillmentText' => $message
+    );
+   $aFinalDialogflowResponse = json_encode($data);
+
+   echo $aFinalDialogflowResponse;
+  }
 
   
 ?>
