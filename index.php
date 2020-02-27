574407327736bca69f456d6b1c84a1b3b2c367ca
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 13:08:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 $data = array();
   $data = array (
   
-  'queryResult' => 
-  array (
-    
     'fulfillmentText' => 'Please enter the Valid Mobile Number ?'
-    )
+   
 );
 $a = json_encode($data);
 
