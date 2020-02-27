7ca87aaa9d9a138ac9b0e486bc0912654bfc64db
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 16:37:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
   $account_number = $requestDecode->queryResult->parameters->Account_Number;
 
   if($intent == "BalanceRequest - yes - AccountNumber"){
-    
+    if($account_number == "456123"){
+      $message = "Please Enter mobile number associated with account";
+    }else{
+    $message = "Sorry we could not found any details against this account number";
+    }
+    $data = array (
+      'fulfillmentText' => $message
+    );
+   $aFinalDialogflowResponse = json_encode($data);
+
+   echo $aFinalDialogflowResponse;
   }
 
-  if($account_number == "456123"){
-  	$message = "Please Enter mobile number associated with account";
-  }else{
- 	$message = "Sorry we could not found any details against this account number";
-  }
-  $data = array (
-  	'fulfillmentText' => $message
-  );
- $aFinalDialogflowResponse = json_encode($data);
   
- echo $aFinalDialogflowResponse;
 ?>
