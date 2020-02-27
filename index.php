e06b04885cd5eaeef6a9465681a370358e2d9ce9
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 14:02:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
  $json = file_get_contents('php://input');
   $b = json_decode($json);
   $account_number = $b->queryResult->parameters->Account_Number;
- 
+ if($account_number == "456123"){
+   $message = "Please Enter mobile number associated with account";
+ }else{
+  $message = "Sorry we could not found any details against this account number";
+ }
   $data = array (
    'fulfillmentText' => $account_number
   );
   $a = json_encode($data);
+  
   print_r($a);
 ?>
