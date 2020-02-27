fffef00399c89e5db283303cb53c6791638a8d35
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 18:22:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,15 +14,13 @@
 
   $request = file_get_contents('php://input');
   $requestDecode = json_decode($request);
- // echo"<pre>";print_r($requestDecode);exit;
-  $intent = $requestDecode->queryResult->intent->displayName;
-  $account_number = $requestDecode->queryResult->parameters->Account_Number;
  
+  $intent = $requestDecode->queryResult->intent->displayName;
   
-
-
   if($intent == "BalanceRequest - yes - AccountNumber"){
+    $account_number = $requestDecode->queryResult->parameters->Account_Number;
     $account_number = str_replace(' ', '', $account_number);
+
     $sql = "SELECT CONCAT(vcd.firstname,' ',vcd.lastname) AS name , vcscf.cf_864 as account_balance FROM vtiger_contactdetails vcd JOIN vtiger_crmentity vce ON vcd.contactid=vce.crmid JOIN vtiger_contactscf vcscf ON vcd.contactid=vcscf.contactid WHERE vce.deleted=0 AND vcscf.cf_856='$account_number' ORDER BY vcd.contactid DESC";
     
     $result = $conn->query($sql);
@@ -34,6 +32,22 @@
       $message = "Please Enter mobile number associated with account";
     }else{
       $message = "Sorry we could not found any details against this account number";
+    }
+
+  }else if($intent == "BalanceRequest - yes - AccountNumber - PhoneNumber"){
+
+    $mobile_number = $requestDecode->queryResult->parameters->Contact;
+
+    $sql = "SELECT CONCAT(vcd.firstname,' ',vcd.lastname) AS name , vcscf.cf_864 as account_balance FROM vtiger_contactdetails vcd JOIN vtiger_crmentity vce ON vcd.contactid=vce.crmid JOIN vtiger_contactscf vcscf ON vcd.contactid=vcscf.contactid WHERE vce.deleted=0 AND vcd.mobile='$mobile_number'ORDER BY vcd.contactid DESC";
+    //echo $sql;exit;
+    $result = $conn->query($sql);
+    while($row =mysqli_fetch_assoc($result)) {
+      $data[] = $row;
+    }
+    if(count($data) == 0){
+      $message = "Sorry we could not found any details against this account number and mobile number.";
+    }else{
+      $message = "Dear ".$data[0]['name'] . " your account balance is ".$data[0]['account_balance'];
     }
   }
   // Dialogflow Response
