315cb6143fe8b0eb3bcb565402b3a24609854352
tushardimble <dimbletushar7004@gmail.com>
2020-02-27 13:47:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   $json = file_get_contents('php://input');
   $b = json_decode($json);
-  $account_number = $b->queryResult->parameters->Account_Number;
+  //$account_number = $b->queryResult->queryText;
   $data = array();
   $data = array (
    'fulfillmentText' => "account number ";
