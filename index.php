356b90d8cbd449be4bb19d4774bcac0e84d61f34
tushardimble <dimbletushar7004@gmail.com>
2020-04-11 14:26:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -533,16 +533,16 @@
   	// Send Respose to Dialogflow fulfillment
   	
   	if($intent=="debit card"){
-  		// $message[0]['platform']="ACTIONS_ON_GOOGLE";
-  		// $message[0]['linkOutSuggestion']['destinationName']="test";
-  		// $message[0]['linkOutSuggestion']['uri']="https://audiodemo-ftnluv.web.app/";
-  		// $message[1]['text']['text'][0]="A Debit Card is personal card provided to the customers by the Bank to enable the card holder to withdraw money or to have the cost of purchases charged directly to the holder's bank account.";
-  		$message[0]['buttons'][0]['postback']="https://audiodemo-ftnluv.web.app/";
-  		$message[0]['buttons'][0]['text']="Open";
-  		$message[0]['imageUrl']="http://urltoimage.com";
   		$message[0]['platform']="ACTIONS_ON_GOOGLE";
-  		$message[0]['title']="Card Title";
-  		$message[0]['type']=1;
+  		$message[0]['linkOutSuggestion']['destinationName']="test";
+  		$message[0]['linkOutSuggestion']['uri']="https://audiodemo-ftnluv.web.app/";
+  		$message[1]['text']['text'][0]="A Debit Card is personal card provided to the customers by the Bank to enable the card holder to withdraw money or to have the cost of purchases charged directly to the holder's bank account.";
+  		// $message[0]['buttons'][0]['postback']="https://audiodemo-ftnluv.web.app/";
+  		// $message[0]['buttons'][0]['text']="Open";
+  		// $message[0]['imageUrl']="http://urltoimage.com";
+  		// $message[0]['platform']="ACTIONS_ON_GOOGLE";
+  		// $message[0]['title']="Card Title";
+  		// $message[0]['type']=1;
   	}
   	$data = array (
     	'fulfillmentText' => $message
