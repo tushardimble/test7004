0ff566444860b40e5f6babcae6a8d0e4cda6b385
tushardimble <dimbletushar7004@gmail.com>
2020-05-19 13:38:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-	$data['payload']['text'] = "Hi, need resend";
-
-    	$aFinalDialogflowResponse = json_encode($data,JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
-
-    echo $aFinalDialogflowResponse;
+	echo "Got response";
 ?>
