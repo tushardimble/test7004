44a0f88fc248a062f8b519df7726d3f834d2eb5a
tushardimble <dimbletushar7004@gmail.com>
2020-05-19 13:36:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-	$data = array (
-      "Hi this is from webhook"
-    );
+	$data['payload']['text'] = "Hi, need resend";
 
-    $aFinalDialogflowResponse = json_encode($data,JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+    	$aFinalDialogflowResponse = json_encode($data,JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
 
     echo $aFinalDialogflowResponse;
 ?>
