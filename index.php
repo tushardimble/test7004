95714a10ad739717d52ea0b7d1554a0a5386ac1d
tushardimble <dimbletushar7004@gmail.com>
2020-05-19 13:42:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-	echo "Got response";
+	echo "Hi, I am Volt. <br >Welcome to Voltas. How may I help you? <br >Please choose from the below options<br><button class='btn-sm btn-primary'>I am a Customer</button>";
 ?>
