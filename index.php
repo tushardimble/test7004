3c0e2ea23e4d0bdc24934dfeb17d0456dd3c71aa
tushardimble <dimbletushar7004@gmail.com>
2020-05-19 13:28:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-	echo"Hi";
+	$data = array (
+      'fulfillmentText' => "Hi this is from webhook"
+    );
+
+    $aFinalDialogflowResponse = json_encode($data,JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+
+    echo $aFinalDialogflowResponse;
 ?>
