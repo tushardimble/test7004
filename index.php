139cbe59a7a1a62cef58f0950e4d6849c45e4e1f
tushardimble <dimbletushar7004@gmail.com>
2020-05-19 13:29:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	$data = array (
-      'fulfillmentText' => "Hi this is from webhook"
+      "Hi this is from webhook"
     );
 
     $aFinalDialogflowResponse = json_encode($data,JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
