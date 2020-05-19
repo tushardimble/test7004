91304f6e99644f91e02471b58138a8488f87ae1b
tushardimble <dimbletushar7004@gmail.com>
2020-05-19 14:06:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-	echo "Hi, I am Volt. <br >Welcome to Voltas. How may I help you? <br >Please choose from the below options<br><button class='btn-sm btn-primary'>I am a Customer</button>";
+	$request        = file_get_contents('php://input');
+    	$requestDecode  = json_decode($request);
+	echo"<pre>";print_r($requestDecode);exit;
+
 ?>
