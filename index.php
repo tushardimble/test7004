b6d7fbb3b7eede7691c855d5c1658ed9f29fcb71
tushardimble <dimbletushar7004@gmail.com>
2020-05-20 19:10:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,13 @@
   error_reporting(0);
   date_default_timezone_set('Asia/Calcutta');
   // DB Credentials which is hosting on Plesk Server
-  $servername = "66.45.232.178";
-    $username   = "axisbankcrm3";
-    $password   = "axisbankcrm3";
+  //$servername = "66.45.232.178";
+    //$username   = "axisbankcrm3";
+    //$password   = "axisbankcrm3";
+
+    $servername = "13.71.118.35";
+    $username   = "root";
+    $password   = "Amri@951753";
     $dbname   = "axisbankcrm3";
     $data     = array();
     // Connect with Db 
